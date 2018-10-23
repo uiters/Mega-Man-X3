eb0f3c5b1b44297eb019e0b09b8bca7acad7303d
ThienLan <meomeocf98@gmail.com>
2018-10-23 22:11:38 +0700

    add debugs

---
+++ MegaManX3/Debugs.h
@@ -0,0 +1,22 @@
+#define _debug.h_
+#ifdef _debug.h_
+
+#include <Windows.h>
+#include <signal.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
+#include <stdlib.h>
+
+void DebugOut(wchar_t *fmt, ...)
+{
+	va_list argp;
+	va_start(argp, fmt);
+	wchar_t dbg_out[4096];
+	vswprintf_s(dbg_out, fmt, argp);
+	va_end(argp);
+	OutputDebugString(dbg_out);
+}
+
+#endif // _debug.h_
